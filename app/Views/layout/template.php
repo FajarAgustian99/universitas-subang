e2e07807bef850bd1e4e18a9cc058900bc63cf2a
FajarAgustian99 <151993356+FajarAgustian99@users.noreply.github.com>
2025-01-01 18:54:39 +0700

    first commit

---
+++ app/Views/layout/template.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap 5 Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url('assets/css/main.css') ?>" />
+
+    <title>$data</title>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container px-5">
+        <a class="navbar-brand" href="#!">
+            <img src="assets/img/logo-unsub.png" alt="" class="logo" width="30" Height="30">
+            UNIVERSITAS SUBANG
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Beranda</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">Profil</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">Akademik & Kemahasiswaan</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">Informasi</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">PMB</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">Lembaga & UPT</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<?= $this->rendersection('content'); ?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-sm-4">
+            <h5>Alamat</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+            <h5>Kontak</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+            <h5>Maps</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
---
+++ app/Views/pages/beranda.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<img src="..." class="img-fluid" alt="...">
+<div class="container-fluid p-5 bg-white text-black text-center">
+    <h1>My First Bootstrap Page</h1>
+    <p>Resize this responsive page to see the effect!</p>
+</div>
+
+<?= $this->endSection(); ?>
